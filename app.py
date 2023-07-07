a046f4eee6b0b948f4b09c8cdd4270374bcaee52
david cheung <107241896+davidcheung6331@users.noreply.github.com>
2023-07-07 17:16:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,59 +5,68 @@ import pandas as pd
 from sqlalchemy import create_engine
 from sqlalchemy.pool import StaticPool
 from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
+from PIL import Image
+
+# requirements
+# streamlit
+# langchain
+# openai
+# pandas
+# SQLAlchemy
+
+st.set_page_config(
+    page_title="CSV to Database Query",
+    page_icon="🗄",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+hide_menu_style = """
+        <style>
+        #MainMenu {visibility: hidden;}
+        </style>
+        """
+
+st.markdown(hide_menu_style, unsafe_allow_html=True)
+image = Image.open("customer-services.png")
+st.image(image, caption='created by MJ')
 
 
-#####################################
-#            FUNCTIONS              #
-#####################################
+# Cache the database and return dataframe by CSV file
 @st.cache_data()
 def load_data(url):
-    """
-    load data from url
-    """
     df = pd.read_csv(url)
     return df
 
+# Create the columns 
 def prepare_data(df):
-    """
-    lowercase columns
-    """
     df.columns = [x.replace(' ', '_').lower() for x in df.columns]
     return df
 
 
-#####################################
-#        LOCALS & CONSTANTS         #
-#####################################
-table_name = 'statesdb'
+table_name = 'db1'
 uri = "file::memory:?cache=shared"
+st.title('🔍  :blue[Step 1: Query your CSV  by SqlDb]')
 
-
-#####################################
-#            HOME PAGE              #
-#####################################
-st.title('ChatGPT Pandas CSV Streamlit :house:')
-st.subheader('Upload a file to query')
-
-# read file
-uploaded_file = st.file_uploader("Choose a csv file")
+# display the content of CSV FILES
+uploaded_file = st.file_uploader("Choose a CSV file to upload")
 if uploaded_file is not None:
     df = pd.read_csv(uploaded_file)
     st.write(df)
 
-    # api key
+    st.title('🔍   :blue[Step 2: Enter your Query Text]')
     openai_api_key = st.text_input(
-        "API key", 
+        ":key: OpenAI Key :", 
         placeholder='1234567890',
         type='password',
         disabled=False,
-        help='Enter your OpenAI api key.'
+        help='Please enter your OpenAI api key.'
     )
 
     # user query
     user_q = st.text_input(
-        "User Query", 
-        help="Enter a question based on the dataset")
+        "your Query Text : ", 
+        help="Enter a question based on the CSV file")
 
     # commit data to sql
     data = prepare_data(df)
@@ -82,4 +91,4 @@ if uploaded_file is not None:
     # run query and display result
     if openai_api_key and user_q:
         result = db_chain.run(user_q)
-        st.write(result)
+        st.info(result)
